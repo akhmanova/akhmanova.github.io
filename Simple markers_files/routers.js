c695e8452c0bf608630bacd9738b28a6b445d6d7
akhmanova <eakhmanova@gmail.com>
2017-02-02 12:24:46 +0300

    second commit

---
+++ Simple markers_files/routers.js
@@ -0,0 +1,64 @@
+var routers={};
+routers['A104-7c6474']={
+ "location": {
+  "lat": 37.5571359,
+  "lng": -121.96863389999999
+ },
+ "accuracy": 57.0
+};
+routers['G306-7cc77c']={
+ "location": {
+  "lat": 37.5552682,
+  "lng": -121.96861979999998
+ },
+ "accuracy": 48.0
+};
+routers['I204-7c5494']={
+ "location": {
+  "lat": 37.55545,
+  "lng": -121.96983449999999
+ },
+ "accuracy": 68.0
+};
+routers['K-213-7c91b4']={
+ "location": {
+  "lat": 37.5559808,
+  "lng": -121.96956150000001
+ },
+ "accuracy": 39.0
+};
+routers['L308-7cafcc']={
+ "location": {
+  "lat": 37.5558481,
+  "lng": -121.96925820000001
+ },
+ "accuracy": 49.0
+};
+routers['M203-7c6da4']={
+ "location": {
+  "lat": 37.5560846,
+  "lng": -121.96886599999999
+ },
+ "accuracy": 27.0
+};
+routers['M207-7c9d14']={
+ "location": {
+  "lat": 37.5557571,
+  "lng": -121.9688088
+ },
+ "accuracy": 54.0
+};
+routers['N305-7ba13c']={
+ "location": {
+  "lat": 37.5558268,
+  "lng": -121.96833649999999
+ },
+ "accuracy": 119.0
+};
+routers['k317-7aee64']={
+ "location": {
+  "lat": 37.5562843,
+  "lng": -121.9695617
+ },
+ "accuracy": 64.0
+};
