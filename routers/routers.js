0a7310e59189a1861748d90e6d3d05539abb8ab3
akhmanova <eakhmanova@gmail.com>
2017-03-03 21:46:24 +0300

    fixed bugs

--- routers/routers.js
+++ routers/routers.js
@@ -1,2 +1,8 @@
 var routers={};
-routers['rpi3-gd-feb21-demo']=;
+routers['rpi3-gd-feb21-demo']={
+ "location": {
+  "lat": 51.535555099999996,
+  "lng": 46.009806999999995
+ },
+ "accuracy": 37.0
+};
